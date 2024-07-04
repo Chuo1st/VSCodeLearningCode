176ddcdb0d0ee2eac71c67a0560cc1c9c2b3baa9
chuo <799566450@qq.com>
2024-07-04 17:01:35 +0800

    AJAX学习，Day03

--- FrontEndLearning/LearnAjax/day2/5.个人信息设置/js/index.js
+++ FrontEndLearning/LearnAjax/day2/5.个人信息设置/js/index.js
@@ -4,36 +4,66 @@
  *  1.2 回显数据到标签上
  * */
 
-// 获取本地存储数据，先吧json转换为js复杂类型，该类型的属性对应的就是input的class
-const localData = JSON.parse(localStorage.getItem('localdata'))
-const url = localStorage.getItem('avatar')
-const avatar = url ===  null ? null : `url(${url})`
-render(localData)
-
-function render(data, av)
+// 渲染表单
+function render()
 {
-    // 遍历data的属性，从而找到相对应的input的dom对象
-    if (data)
-    {
-        const keys = Object.keys(data)
-        for (let i = 0; i < keys.length; ++i)
-        {
-            const input = document.querySelector(`.${keys[i]}`)
-            input.value = data[keys[i]]
-        }
-    }
+    axios({url:'https://hmajax.itheima.net/api/settings', params:{creator:'chuo'}}).then(result => {
+        console.log(result)
+        const data = result.data.data
+        Object.keys(data).forEach(key =>{
+            if (key === 'avatar')
+            {
+                const prew = document.querySelector('.prew')
+                prew.src = data[key]
+            }
+            else if (key === 'gender')
+            {
+                const ratioButtons = document.querySelectorAll('.gender')
+                console.log(ratioButtons)
+                ratioButtons[data[key]].checked = true
+            }
+            else{
+                const inputDom = document.querySelector(`.${key}`)
+                inputDom.value = data[key]
+            }
+        })
+    }).catch(err =>{
+        console.log(err)
+    })
+}
+render()
 
-    if (av)
+// 获取提示框
+const toastDom = document.querySelector()
+const toast = new bootstrap.Toast(toastDom)
+funtion toastShow(message, isSuccessfully)
+{
+    if (isSuccessfully)
     {
-        const imgBox = document.querySelector('.prew')
-        imgBox.src = av
+        toastDom.className = "alert alert-success"
+    }else{
+        toastDom.className = "alert alert-danger"
     }
 }
 
-// 头像设置模块
-const changeAvatarDom = document.querySelector('.upload')
-changeAvatarDom.addEventListener('change', (e)=>{
-    // 获取图片
-    const
-    const selectedAvatar = e.target.files[0] 
+// 头像上传模块
+const uploadBtn = document.querySelector('.upload')
+uploadBtn.addEventListener('change', e => {
+    const pic = new FormData()
+    pic.append('avatar', e.target.files[0])
+    pic.append('creator', 'chuo')
+    axios({url:'https://hmajax.itheima.net/api/avatar', method:'put', data:pic}).then(result => {
+        const prew = document.querySelector('.prew')
+        prew.src = result.data.data.avatar
+    }).catch(err => console.log(err))
+})
+
+// 修改个人信息
+const submitBtn = document.querySelector('.submit')
+submitBtn.addEventListener('click', () => {
+    const form = document.querySelector('.user-form')
+    const data = serialize(form, {hash:true, empty:true})
+    axios({url:'https://hmajax.itheima.net/api/settings', method:'put', data:{desc:data.desc, email:data.email, gender:+data.gender, nickname:data.nickname, creator:'chuo'}}).then(result => {
+        console.log(result)
+    }).catch(err => console.log(err))
 })
